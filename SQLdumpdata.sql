4058ff033d081f5e1d048d1c578f2d1e8d4b776a
Asher <51806917+asher-lab@users.noreply.github.com>
2021-07-11 09:37:14 +0800

    Create SQLdumpdata.sql

---
+++ SQLdumpdata.sql
@@ -0,0 +1,79 @@
+insert into custinfo_tb (transactionID, Col_ID, email, contact, address, tdcp, pc) values (1, 20190000, 'jdoumerque0@auda.org.au', 0926702804, 'Manila', '55 Transport Drive', '1000');
+insert into custinfo_tb (transactionID, Col_ID, email, contact, address, tdcp, pc) values (2, 20190001, 'jboughen1@ovh.net', 0909702814, 'Quezon City', '6125 Stang Circle', '1001');
+insert into custinfo_tb (transactionID, Col_ID, email, contact, address, tdcp, pc) values (3, 20190002, 'ishowers2@devhub.com', 0945728014, 'Subic Bay', '54918 Alpine Place', '1002');
+insert into custinfo_tb (transactionID, Col_ID, email, contact, address, tdcp, pc) values (4, 20190003, 'cbackhurst3@hostgator.com', 0991728014, 'Rizal', '90340 Sugar Drive', '1003');
+insert into custinfo_tb (transactionID, Col_ID, email, contact, address, tdcp, pc) values (5, 20190004, 'scrank4@techcrunch.com', 0995708014, 'Siquijor', '913 Russell Avenue', '1004');
+insert into custinfo_tb (transactionID, Col_ID, email, contact, address, tdcp, pc) values (6, 20190005, 'mklimczak5@issuu.com', 0917702014, 'Zambales', '8827 Jenifer Trail', '1005');
+insert into custinfo_tb (transactionID, Col_ID, email, contact, address, tdcp, pc) values (7, 20190006, 'jantoinet6@huffingtonpost.com', 0916728014, 'Bataan', '96301 Ryan Trail', '1006');
+insert into custinfo_tb (transactionID, Col_ID, email, contact, address, tdcp, pc) values (8, 20190007, 'epawlyn7@house.gov', 0916702033, 'Pampanga', '47 Eastwood Lane', '1007');
+insert into custinfo_tb (transactionID, Col_ID, email, contact, address, tdcp, pc) values (9, 20190008, 'ssyrie8@usa.gov', 0923402814, 'Clark', '96992 Maywood Junction', '1008');
+insert into custinfo_tb (transactionID, Col_ID, email, contact, address, tdcp, pc) values (10, 20190009, 'madamovsky9@bing.com', 0990228014, 'Manila', '4 Moland Crossing', '1009');
+insert into custinfo_tb (transactionID, Col_ID, email, contact, address, tdcp, pc) values (11, 20190010, 'jlindwalla@phpbb.com', 0910928014, 'Subic Bay', '85213 Roth Junction', '1010');
+insert into custinfo_tb (transactionID, Col_ID, email, contact, address, tdcp, pc) values (12, 20190011, 'smcgoldrickb@wikispaces.com', 0993028014, 'Subic Bay', '12 Namekagon Street', '1011');
+insert into custinfo_tb (transactionID, Col_ID, email, contact, address, tdcp, pc) values (13, 20190012, 'pricardinc@51.la', 0906102014, 'Subic Bay', '6369 Algoma Court', '1012');
+
+
+insert into brokerinfo_tb (transactionID, Brokerid, Broker_contact, Account_wBroker) values (1, 'ABACUS', 6329251605, 'Jonas Merque');
+insert into brokerinfo_tb (transactionID, Brokerid, Broker_contact, Account_wBroker) values (2, 'ABACUS', 6329251605, 'John Brendon');
+insert into brokerinfo_tb (transactionID, Brokerid, Broker_contact, Account_wBroker) values (3, 'BDO', 6329252222, 'Ilana Magwire');
+insert into brokerinfo_tb (transactionID, Brokerid, Broker_contact, Account_wBroker) values (4, 'FIRSTMETRO', 6329253333, 'Charles Benavidez');
+insert into brokerinfo_tb (transactionID, Brokerid, Broker_contact, Account_wBroker) values (5, 'ABACUS', 6329251605, 'Leonardo Duterte');
+insert into brokerinfo_tb (transactionID, Brokerid, Broker_contact, Account_wBroker) values (6, 'FIRSTMETRO', 6329253333, 'Leonor Roque');
+insert into brokerinfo_tb (transactionID, Brokerid, Broker_contact, Account_wBroker) values (7, 'BDO', 6329252222, 'Danny Malakas');
+insert into brokerinfo_tb (transactionID, Brokerid, Broker_contact, Account_wBroker) values (8, 'BARCELON', 6329251111, 'Denalego Cruz');
+insert into brokerinfo_tb (transactionID, Brokerid, Broker_contact, Account_wBroker) values (9, 'BARCELON', 6329251111, 'Rebine Lean Galacgac');
+insert into brokerinfo_tb (transactionID, Brokerid, Broker_contact, Account_wBroker) values (10, 'MANDARIN', 6329001605, 'Arwin Racan');
+insert into brokerinfo_tb (transactionID, Brokerid, Broker_contact, Account_wBroker) values (11, 'MANDARIN', 6329001605, 'Laurence Caesar');
+insert into brokerinfo_tb (transactionID, Brokerid, Broker_contact, Account_wBroker) values (12, 'MANDARIN', 6329001605, 'Zyra Mae Galindo');
+insert into brokerinfo_tb (transactionID, Brokerid, Broker_contact, Account_wBroker) values (13, 'WEALTH', 6329001605, 'Anna Cleto');
+
+
+	
+insert into stockinfo_tb (transactionID, Stock_Code, Stock_Name, Stock_Amount, Avecost, Cert_amount) values (1, 'AR', 'Abra Mining', 90000000, 0.0046, 0);
+insert into stockinfo_tb (transactionID, Stock_Code, Stock_Name, Stock_Amount, Avecost, Cert_amount) values (2, 'AR', 'Abra Mining', 10000000, 0.01, 0);
+insert into stockinfo_tb (transactionID, Stock_Code, Stock_Name, Stock_Amount, Avecost, Cert_amount) values (3, 'AB', 'Atok Big Wedge', 6500, 5.45, 0);
+insert into stockinfo_tb (transactionID, Stock_Code, Stock_Name, Stock_Amount, Avecost, Cert_amount) values (4, 'AB', 'Atok Big Wedge', 17000, 10.45, 0);
+insert into stockinfo_tb (transactionID, Stock_Code, Stock_Name, Stock_Amount, Avecost, Cert_amount) values (5, 'JFC', 'Jollibee Food Corporation', 400, 150, 0);
+insert into stockinfo_tb (transactionID, Stock_Code, Stock_Name, Stock_Amount, Avecost, Cert_amount) values (6, 'PHA', 'Premiere Horizon Alliance Corp', 90000, 3, 0);
+insert into stockinfo_tb (transactionID, Stock_Code, Stock_Name, Stock_Amount, Avecost, Cert_amount) values (7, 'PHA', 'Premiere Horizon Alliance Corp', 180000, 1.37, 0);
+insert into stockinfo_tb (transactionID, Stock_Code, Stock_Name, Stock_Amount, Avecost, Cert_amount) values (8, 'VUL', 'Vulcan Industrial Corporation', 1000, 2.27, 0);
+insert into stockinfo_tb (transactionID, Stock_Code, Stock_Name, Stock_Amount, Avecost, Cert_amount) values (9, 'AR', 'Abra Mining', 55000000, 0.0027, 0);
+insert into stockinfo_tb (transactionID, Stock_Code, Stock_Name, Stock_Amount, Avecost, Cert_amount) values (10, 'VUL', 'Vulcan Industrial Corporation', 25000, 3.12 , 0);
+insert into stockinfo_tb (transactionID, Stock_Code, Stock_Name, Stock_Amount, Avecost, Cert_amount) values (11, 'GMA7', 'GMA Network Inc', 11000, 7 , 0);
+insert into stockinfo_tb (transactionID, Stock_Code, Stock_Name, Stock_Amount, Avecost, Cert_amount) values (12, 'ABS', 'ABS CBN Corporation', 600, 5, 0);
+insert into stockinfo_tb (transactionID, Stock_Code, Stock_Name, Stock_Amount, Avecost, Cert_amount) values (13, 'GLO', 'Globe Telecom Inc', 10000, 2100, 0);
+
+
+  
+insert into transactiontype_tb ( transactionID, Transfertype, transferdesc, transfee, transperiod) values (1, 'Stock Transfer Out', 'Transfer IN/OUT', 75, '2 days');
+insert into transactiontype_tb ( transactionID, Transfertype, transferdesc, transfee, transperiod) values (2, 'Stock Transfer Out', 'Transfer IN/OUT', 75, '2 days');
+insert into transactiontype_tb ( transactionID, Transfertype, transferdesc, transfee, transperiod) values (3, 'Stock Transfer Out', 'Transfer IN/OUT', 75, '2 days');
+insert into transactiontype_tb ( transactionID, Transfertype, transferdesc, transfee, transperiod) values (4, 'Stock Transfer In', 'Transfer IN/OUT', 75, '2 days');
+insert into transactiontype_tb ( transactionID, Transfertype, transferdesc, transfee, transperiod) values (5, 'Stock Transfer In', 'Transfer IN/OUT', 75, '2 days');
+insert into transactiontype_tb ( transactionID, Transfertype, transferdesc, transfee, transperiod) values (6, 'Stock Transfer Out', 'Transfer IN/OUT', 75, '2 days');
+insert into transactiontype_tb ( transactionID, Transfertype, transferdesc, transfee, transperiod) values (7, 'Stock Transfer In', 'Transfer IN/OUT', 75, '2 days');
+insert into transactiontype_tb ( transactionID, Transfertype, transferdesc, transfee, transperiod) values (8, 'Stock Transfer Out', 'Transfer IN/OUT', 75, '2 days');
+insert into transactiontype_tb ( transactionID, Transfertype, transferdesc, transfee, transperiod) values (9, 'Stock Transfer In', 'Transfer IN/OUT', 75, '2 days');
+insert into transactiontype_tb ( transactionID, Transfertype, transferdesc, transfee, transperiod) values (10, 'Stock Transfer Out', 'Transfer IN/OUT', 75, '2 days');
+insert into transactiontype_tb ( transactionID, Transfertype, transferdesc, transfee, transperiod) values (11, 'Stock Transfer In', 'Transfer IN/OUT', 75, '2 days');
+insert into transactiontype_tb ( transactionID, Transfertype, transferdesc, transfee, transperiod) values (12, 'Stock Transfer Out', 'Transfer IN/OUT', 75, '2 days');
+insert into transactiontype_tb ( transactionID, Transfertype, transferdesc, transfee, transperiod) values (13, 'Stock Transfer In', 'Transfer IN/OUT', 75, '2 days');
+
+
+
+
+
+
+insert into transaction_tb ( transactionID, Col_ID, sysdate, Stock_Code, Brokerid, TransferType) values (1, 20190000, NOW(), 'AR','ABACUS','Stock Transfer Out');
+insert into transaction_tb ( transactionID, Col_ID, sysdate, Stock_Code, Brokerid, TransferType) values (2, 20190001, NOW() , 'AR','ABACUS','Stock Transfer Out');
+insert into transaction_tb ( transactionID, Col_ID, sysdate, Stock_Code, Brokerid, TransferType) values (3, 20190002, NOW(), 'AB','BDO','Stock Transfer Out');
+insert into transaction_tb ( transactionID, Col_ID, sysdate, Stock_Code, Brokerid, TransferType) values (4, 20190003, NOW(), 'AB','FIRSTMETRO','Stock Transfer In');
+insert into transaction_tb ( transactionID, Col_ID, sysdate, Stock_Code, Brokerid, TransferType) values (5, 20190004, NOW(), 'JFC','ABACUS','Stock Transfer In');
+insert into transaction_tb ( transactionID, Col_ID, sysdate, Stock_Code, Brokerid, TransferType) values (6, 20190005, NOW(), 'PHA','FIRSTMETRO','Stock Transfer Out');
+insert into transaction_tb ( transactionID, Col_ID, sysdate, Stock_Code, Brokerid, TransferType) values (7, 20190006, NOW(), 'PHA','BDO','Stock Transfer In');
+insert into transaction_tb ( transactionID, Col_ID, sysdate, Stock_Code, Brokerid, TransferType) values (8, 20190007, NOW(), 'VUL','BARCELON','Stock Transfer Out');
+insert into transaction_tb ( transactionID, Col_ID, sysdate, Stock_Code, Brokerid, TransferType) values (9,  20190008, NOW(), 'AR','BARCELON','Stock Transfer In');
+insert into transaction_tb ( transactionID, Col_ID, sysdate, Stock_Code, Brokerid, TransferType) values (10, 20190009, NOW(), 'VUL','MANDARIN','Stock Transfer Out');
+insert into transaction_tb ( transactionID, Col_ID, sysdate, Stock_Code, Brokerid, TransferType) values (11, 20190010, NOW(), 'GMA7','MANDARIN','Stock Transfer In');
+insert into transaction_tb ( transactionID, Col_ID, sysdate, Stock_Code, Brokerid, TransferType) values (12, 20190011, NOW(), 'ABS','MANDARIN', 'Stock Transfer Out');
+insert into transaction_tb ( transactionID, Col_ID, sysdate, Stock_Code, Brokerid, TransferType) values (13, 20190012, NOW(), 'GLO','WEALTH','Stock Transfer In');
