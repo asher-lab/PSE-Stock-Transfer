44000ba490575bd7d115bd20c48839e01e392284
Asher <51806917+asher-lab@users.noreply.github.com>
2021-07-11 21:15:50 +0800

    Update SQLcreatetable.sql

--- SQLcreatetable.sql
+++ SQLcreatetable.sql
@@ -1,8 +1,9 @@
+
 CREATE TABLE custinfo_tb(
 	transactionID bigint NOT NULL,
     Col_ID int(8) NOT NULL,
     email varchar(50) NOT NULL,
-	contact int(11) NOT NULL,
+	contact bigint NOT NULL,
 	address varchar(100) NOT NULL,
 	tdcp varchar(100) NOT NULL,
 	pc int(4) NOT NULL,
@@ -16,7 +17,7 @@ CREATE TABLE brokerinfo_tb
 (
  transactionID bigint NOT NULL,
   Brokerid varchar(30) NOT NULL,
-  Broker_contact bigint(15) NOT NULL,
+  Broker_contact bigint NOT NULL,
   Account_wBroker varchar(100) NOT NULL,
   PRIMARY KEY (Brokerid,transactionID)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
